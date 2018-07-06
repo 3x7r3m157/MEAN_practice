9fcf576032024aef6b02a861be375f359335d842
Austin Atendido <austin@hackernest.com>
2018-07-06 15:10:39 -0700

    learning routing in Express...woot woot

---
+++ Express.js/cars_cats/server.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var app = express();
+
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
+
+app.use(express.static(__dirname + "/static"))
+
+app.get('/', function(request, response) {
+    response.send("<h1>Hit root.</h1>")
+})
+
+app.get('/cars', function(request, response) {
+    response.render('cars')
+})
+
+app.get('/cats', function(request, response) {
+    response.render('cats')
+})
+
+app.get('/cats/new', function(request, response) {
+    response.render('new')
+})
+
+app.listen(8000, function() {
+    console.log('king me.')
+})
